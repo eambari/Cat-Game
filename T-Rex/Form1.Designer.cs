13e443b92898593c076ec8dca0e97acb0621beb5
enluxX <endritambari@gmail.com>
2023-08-16 18:09:06 +0200

    Added doublebuffered, created InitializeObstacle function, created a list of obstacles for more obsticles to be shown.

--- T-Rex/Form1.Designer.cs
+++ T-Rex/Form1.Designer.cs
@@ -31,37 +31,22 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             character = new PictureBox();
-            obstacle = new PictureBox();
             timer = new System.Windows.Forms.Timer(components);
             label = new Label();
             score = new Label();
             ((System.ComponentModel.ISupportInitialize)character).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)obstacle).BeginInit();
             SuspendLayout();
             // 
             // character
             // 
             character.BackColor = Color.Transparent;
             character.Image = (Image)resources.GetObject("character.Image");
-            character.Location = new Point(164, 305);
-            character.Margin = new Padding(3, 2, 3, 2);
+            character.Location = new Point(187, 407);
             character.Name = "character";
-            character.Size = new Size(86, 76);
+            character.Size = new Size(98, 101);
             character.SizeMode = PictureBoxSizeMode.StretchImage;
             character.TabIndex = 0;
             character.TabStop = false;
-            // 
-            // obstacle
-            // 
-            obstacle.BackColor = Color.Transparent;
-            obstacle.Image = (Image)resources.GetObject("obstacle.Image");
-            obstacle.Location = new Point(443, 256);
-            obstacle.Margin = new Padding(3, 2, 3, 2);
-            obstacle.Name = "obstacle";
-            obstacle.Size = new Size(49, 125);
-            obstacle.SizeMode = PictureBoxSizeMode.Zoom;
-            obstacle.TabIndex = 1;
-            obstacle.TabStop = false;
             // 
             // label
             // 
@@ -69,9 +54,9 @@
             label.BackColor = Color.Transparent;
             label.Font = new Font("Tempus Sans ITC", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
             label.ForeColor = Color.White;
-            label.Location = new Point(555, 7);
+            label.Location = new Point(634, 9);
             label.Name = "label";
-            label.Size = new Size(52, 19);
+            label.Size = new Size(64, 24);
             label.TabIndex = 3;
             label.Text = "Score:";
             // 
@@ -81,26 +66,25 @@
             score.BackColor = Color.Transparent;
             score.Font = new Font("Tempus Sans ITC", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
             score.ForeColor = Color.White;
-            score.Location = new Point(616, 7);
+            score.Location = new Point(704, 9);
             score.Name = "score";
-            score.Size = new Size(20, 19);
+            score.Size = new Size(24, 24);
             score.TabIndex = 4;
             score.Text = "0";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(score);
             Controls.Add(label);
-            Controls.Add(obstacle);
             Controls.Add(character);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)character).EndInit();
-            ((System.ComponentModel.ISupportInitialize)obstacle).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -108,7 +92,6 @@
         #endregion
 
         private PictureBox character;
-        private PictureBox obstacle;
         private System.Windows.Forms.Timer timer;
         private Label label;
         private Label score;
--- T-Rex/Form1.cs
+++ T-Rex/Form1.cs
@@ -7,7 +7,6 @@ namespace T_Rex
         private const int FormWidth = 800;
         private const int FormHeight = 450;
         private const int GroundHeight = 72;
-        private const int ObstacleSize = 120;
         private const int JumpHeight = 100;
         private const int ObstacleSpeed = 5;
         private const int TimerInterval = 10;
@@ -16,36 +15,53 @@ namespace T_Rex
             @"C:\Users\Endrit\source\repos\T-Rex\T-Rex\Recources\smallBush.png",
             @"C:\Users\Endrit\source\repos\T-Rex\T-Rex\Recources\bigBush.png"
         };
+        private List<PictureBox> Obstacles;
         private bool isJumping;
         private int jumpHeightRemaining;
+        private int timerTicks;
         private Random random;
         public Form1()
         {
             InitializeComponent();
             random = new Random();
+            this.DoubleBuffered = true;
             Initializer();
         }
-        public void Initializer()
+        private void Initializer()
         {
             Width = FormWidth;
             Height = FormHeight;
 
-            //character.Size = new Size(CharacterSize, CharacterSize);
             character.Location = new Point(100, Height - GroundHeight - character.Image.Height);
             Controls.Add(character);
-
-            obstacle.ImageLocation = ObstacleImages[0];
-            //obstacle.Size = new Size(ObstacleSize, ObstacleSize);
-            obstacle.SizeMode = PictureBoxSizeMode.Zoom;
-            obstacle.Location = new Point(Width, Height - 60 - ObstacleSize);
-            Controls.Add(obstacle);
-
+            Obstacles = new List<PictureBox>();
+            InitializeObstacle();
             timer = new System.Windows.Forms.Timer();
             timer.Interval = TimerInterval;
             timer.Tick += Timer_Tick;
             timer.Start();
 
             KeyDown += MainForm_KeyDown;
+        }
+        private void InitializeObstacle()
+        {
+            var tmp_obstacle = new PictureBox();
+            int listNumber = random.Next(0, 2);
+            if (listNumber == 0)
+            {
+                tmp_obstacle.Location = new Point(Width, Height - 155);
+                tmp_obstacle.Size = new Size(90, 90);
+            }
+            else
+            {
+                tmp_obstacle.Location = new Point(Width, Height - 210);
+                tmp_obstacle.Size = new Size(70, 150);
+            }
+            tmp_obstacle.SizeMode = PictureBoxSizeMode.Zoom;
+            tmp_obstacle.BackColor = Color.Transparent;
+            tmp_obstacle.ImageLocation = ObstacleImages[listNumber];
+            Controls.Add(tmp_obstacle);
+            Obstacles.Add(tmp_obstacle);
         }
         private void MainForm_KeyDown(object sender, KeyEventArgs e)
         {
@@ -62,6 +78,11 @@ namespace T_Rex
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            ++timerTicks;
+            if(timerTicks % 91 == 0)
+            {
+                InitializeObstacle();
+            }
             score.Text = (Convert.ToInt32(score.Text) + 1).ToString();
             if (isJumping)
             {
@@ -83,37 +104,34 @@ namespace T_Rex
                 }
             }
 
-            obstacle.Left -= ObstacleSpeed;
-
-            if (obstacle.Right < 0)
+            for(int i = 0; i < Obstacles.Count; i++)
             {
-                int listNumber = random.Next(0, 2);
-                if(listNumber == 0)
+                Obstacles[i].Left -= ObstacleSpeed;
+                if (Obstacles[i].Right < 0)
                 {
-                    obstacle.Location = new Point(Width, Height - 60 - ObstacleSize);
-                } else
-                {
-                    obstacle.Location = new Point(Width, Height - 90 - ObstacleSize);
+                        Obstacles.RemoveAt(i);
                 }
-                obstacle.ImageLocation = ObstacleImages[listNumber];
-                obstacle.Left = Width;
-            }
 
-            if (character.Bounds.IntersectsWith(obstacle.Bounds))
-            {
-                timer.Stop();
-                maxScore = Math.Max(maxScore, Convert.ToInt32(score.Text));
-                DialogResult result = MessageBox.Show($"Game over!\nYour Score: {score.Text}\nMax Score: {maxScore}\nDo you want to play a new game?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (result == DialogResult.No)
+                if (character.Bounds.IntersectsWith(Obstacles[i].Bounds))
                 {
-                    Application.Exit();
+                    timer.Stop();
+                    maxScore = Math.Max(maxScore, Convert.ToInt32(score.Text));
+                    DialogResult result = MessageBox.Show($"Game over!\nYour Score: {score.Text}\nMax Score: {maxScore}\nDo you want to play a new game?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.No)
+                    {
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        score.Text = "0";
+                        Obstacles.ForEach((obstacle) =>
+                        {
+                            Controls.Remove(obstacle);
+                        });
+                        Initializer();
+                    }
                 }
-                else
-                {
-                    score.Text = "0";
-                    Initializer();
-                }
-            }
+            };
         }
     }
 }
